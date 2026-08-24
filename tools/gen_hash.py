c71c54ebfea29d9d2becd6d4c10cb68902b58605
lzl-svg <1431709672@qq.com>
2026-08-24 15:00:55 +0800

    Delete tools/gen_hash.py

--- tools/gen_hash.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""生成访问口令的加密值：python tools/gen_hash.py 你的口令"""
-import hashlib
-import sys
-
-if len(sys.argv) < 2:
-    print("用法: python tools/gen_hash.py 你的口令")
-else:
-    print(hashlib.sha256(sys.argv[1].encode("utf-8")).hexdigest())
